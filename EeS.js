09baac23297b7b9dc087e9e2d5bcc0ce785cb550
Matthew <bachraty.m@gmail.com>
2022-03-24 17:32:49 -0400

    got the ids working + changing colours

--- EeS.js
+++ EeS.js
@@ -1,6 +1,8 @@
-let numPix = 30;
+let numPix = 15;
 
-
+function showMore(){
+  console.log('test');
+}
 
 function genDivs(v){ 
     var e = document.getElementById("container");
@@ -9,21 +11,26 @@ function genDivs(v){
       row.className = "row"; 
       for(var x = 1; x <= v; x++){ 
           var cell = document.createElement("div"); 
-          cell.className = "gridsquare"; 
+          cell.className = `gridsquare`;
+          cell.setAttribute('id',`${(i*v)+x}`);
+ 
           //cell.innerText = (i * v) + x;
           row.appendChild(cell); 
-          console.log("hello")
+        
       } 
       e.appendChild(row); 
     } 
-    document.getElementById("code").innerText = e.innerHTML;
-
+   
   }
 
   genDivs(numPix)
 
-
-  function draw() {
-
-    
-  }
+ 
+  document.getElementById("container").addEventListener("hover",function(e) {
+    // e.target was the clicked element
+   // if (e.target && e.target.matches("")) {
+    var element = document.getElementById(`${e.target.id}`);
+    element.style.backgroundColor = "#00FF00";
+      console.log(e.target.id);
+   // }
+  });
